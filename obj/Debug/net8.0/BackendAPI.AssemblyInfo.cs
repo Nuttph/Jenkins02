493761cf9c8906da64e6209a766aa550348364b0
Nuttaphon <nathn2420@gmail.com>
2025-05-25 07:52:26 +0700

    Jenkins

--- obj/Debug/net8.0/BackendAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/BackendAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb0cecc94e51564393b7d5ac23987f54c2ae68db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37d6bbee7f2b9246bd18322a01a24f95f3cd276b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
